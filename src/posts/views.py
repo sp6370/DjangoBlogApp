33452fddfe237835bc76a579fc93070f69c6b59e
Sudhanshu Pandey <sp6370@nyu.edu>
2022-01-13 18:53:20 +0000

    Drafts post update feature added

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,18 +1,23 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
 from django.contrib import messages
 from .forms import BlogPostModelForm
 
-#no page error handle
 def posts_list_view(request):
     qs = BlogPost.objects.all().published()
     if request.user.is_authenticated:
         my_qs = BlogPost.objects.filter(user=request.user)
         qs = (qs | my_qs).distinct()
-    template_name = 'list.html'
-    context = {'object_list': qs}
-    return render(request, template_name, context)
+    if qs:
+        template_name = 'list.html'
+        context = {'object_list': qs}
+        return render(request, template_name, context)
+    else:
+        template_name ='detail.html'
+        context = {'object': None}
+        messages.info(request,"No blog posts available.")
+        return render(request, template_name, context)
 
 
 def posts_detail_view(request, slug):
@@ -46,4 +51,24 @@ def posts_create_view(request):
 
 @login_required
 def posts_update_view(request, slug):
-    return HttpResponse("Test") 
+    obj = get_object_or_404(BlogPost, slug=slug)
+    if(obj.user == request.user):
+        if(obj.status == 'D'):
+            form = BlogPostModelForm(request.POST or None, instance=obj)
+            if form.is_valid():
+                form.save()
+            template_name = 'form.html'
+            context = {"title": f"Update {obj.title}", "form": form}
+            return render(request, template_name, context)
+        else:
+            messages.info(request,"Published posts can't be updated.")
+            template_name = 'detail.html'
+            context = {"object": None}
+            return render(request, template_name, context)
+    else:
+        messages.error(request,"Unauthorized Access.")
+        template_name = 'detail.html'
+        context = {"object": None}
+        return render(request, template_name, context) 
+        
+
