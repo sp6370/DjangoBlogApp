e50bc68553423550637f701302fe4aa4ccb5bf11
Sudhanshu Pandey <sp6370@nyu.edu>
2022-01-13 16:51:20 +0000

    Blogpost model added

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+class BlogPost(models.Model): 
+    POST_STATUS = (('D', 'Draft'),('P', 'Publised'))
+    user    = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+    title  = models.CharField(unique = True, max_length=120, blank=False, null = False)
+    slug   = models.SlugField(null=False, unique=True)
+    content  = models.TextField(null=True, blank=False)
+    timestamp = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=1, choices=POST_STATUS, blank=False)
+
+    class Meta:
+        ordering = ['-timestamp']
+
+    def get_absolute_url(self):
+        return f"/blog/{self.slug}"
+
+    def get_edit_url(self):
+        return f"{self.get_absolute_url()}/edit"
+
+    def save(self, *args, **kwargs):
+        value = self.title
+        self.slug = slugify(value, allow_unicode=True)
+        super().save(*args, **kwargs)
